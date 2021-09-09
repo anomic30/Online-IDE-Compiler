56ff8ea133f52140c180399fa0bae1c559d31d66
[Anom Chakravorty] <anomchakravorty3008@gmail.com>
2021-09-09 16:37:34 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Editor from "@monaco-editor/react";
 import Navbar from './Components/Navbar';
@@ -16,7 +16,12 @@ function App() {
   const [userInput, setUserInput] = useState("");
   const [userOutput, setUserOutput] = useState("");
   const [loading, setLoading] = useState(false);
-  
+  const [width, setWidth] = useState(1000);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  },[])
+
   const options = {
     fontSize: fontSize
   }
@@ -41,43 +46,52 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar
-        userLang={userLang} setUserLang={setUserLang}
-        userTheme={userTheme} setUserTheme={setUserTheme}
-        fontSize={fontSize} setFontSize={setFontSize}
-      />
-      <div className="main">
-        <div className="left-container">
-          <Editor
-            options={options}
-            height="calc(100vh - 50px)"
-            width="100%"
-            theme={userTheme}
-            language={userLang}
-            defaultLanguage="python"
-            defaultValue="# Enter your code here"
-            onChange={(value)=> {setUserCode(value)}}
+      {width <= 880 ? (
+        <div className="unavailable">
+          <span className="text-mask">Not available for mobile devices yet!</span>
+        </div>
+      ) : (
+        <>
+          <Navbar
+            userLang={userLang} setUserLang={setUserLang}
+            userTheme={userTheme} setUserTheme={setUserTheme}
+            fontSize={fontSize} setFontSize={setFontSize}
           />
-          <button className="run-btn" onClick={() => compile()}>Run</button>
-        </div>
-        <div className="right-container">
-          <h4>Input:</h4>
-          <div className="input-box">
-            <textarea id="code-inp" onChange={(e)=>setUserInput(e.target.value)}></textarea>
+          <div className="main">
+            <div className="left-container">
+              <Editor
+                options={options}
+                height="calc(100vh - 50px)"
+                width="100%"
+                theme={userTheme}
+                language={userLang}
+                defaultLanguage="python"
+                defaultValue="# Enter your code here"
+                onChange={(value) => { setUserCode(value) }}
+              />
+              <button className="run-btn" onClick={() => compile()}>Run</button>
+            </div>
+            <div className="right-container">
+              <h4>Input:</h4>
+              <div className="input-box">
+                <textarea id="code-inp" onChange={(e) => setUserInput(e.target.value)}></textarea>
+              </div>
+              <h4>Output:</h4>
+              {loading ? (
+                <div className="spinner-box">
+                  <img src={spinner} alt="Loading..." />
+                </div>
+              ) : (
+                <div className="output-box">
+                  <pre>{userOutput}</pre>
+                  <button onClick={() => { clearOutput() }} className="clear-btn">Clear</button>
+                </div>
+              )}
+            </div>
           </div>
-          <h4>Output:</h4>
-          {loading ? (
-            <div className="spinner-box">
-              <img src={spinner} alt="Loading..." />
-            </div>
-          ): (
-            <div className="output-box">
-                <pre>{userOutput}</pre>
-                <button onClick={() => {clearOutput()}} className="clear-btn">Clear</button>
-            </div>
-          )}
-        </div>
-      </div>
+        </>
+      )}
+
     </div>
   );
 }
